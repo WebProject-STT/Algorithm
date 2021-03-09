b73b86000c4f71a7d2c1b38945ecb4c1b43a4d31
Juyoung4 <vallot7@naver.com>
2021-03-09 14:37:12 +0900

    [Edit] ✍ baekjoon_12761.돌다리

--- baekjoon/3주차/12761/12761_jy.py
+++ baekjoon/3주차/12761/12761_jy.py
@@ -2,7 +2,6 @@
 A, B, N, M = map(int, input().split())
 from collections import deque
 
-m = [1, -1, +A, +B, -A, -B, A, B]
 queue = deque([])
 visited = [0 for _ in range(100001)]
 
@@ -10,21 +9,44 @@ def bfs():
     while queue:
         node, count = queue.popleft()
         temp = [node+1, node-1, node+A, node-A, node+B, node-B, node*A, node*B]
-        check = True
         for next_ in temp:
             if 0 <= next_ <= 100000 and visited[next_] == 0:
                 if next_==M:
-                    check = False
+                    return count+1
                 else:
                     visited[next_] = 1
                     queue.append((next_, count+1))
-        if not check:
-            print(count+1)
-            break
             
 queue.append((N, 0))
 visited[N] = 1
-bfs()
+print(bfs())
+
+# A, B, N, M = map(int, input().split())
+# from collections import deque
+
+# m = [1, -1, +A, +B, -A, -B, A, B]
+# queue = deque([])
+# visited = [0 for _ in range(100001)]
+
+# def bfs():
+#     while queue:
+#         node, count = queue.popleft()
+#         temp = [node+1, node-1, node+A, node-A, node+B, node-B, node*A, node*B]
+#         check = True
+#         for next_ in temp:
+#             if 0 <= next_ <= 100000 and visited[next_] == 0:
+#                 if next_==M:
+#                     check = False
+#                 else:
+#                     visited[next_] = 1
+#                     queue.append((next_, count+1))
+#         if not check:
+#             print(count+1)
+#             break
+            
+# queue.append((N, 0))
+# visited[N] = 1
+# bfs()
 
 # 두 개 다 시간 비슷
 
