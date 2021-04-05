85b85131768bd2c7fcf003de6d264130102310b6
Juyoung4 <vallot7@naver.com>
2021-04-06 00:00:15 +0900

    [EDIT] 💌 baekjoon_16236.아기상어

--- baekjoon/8주차/16236/16236_jy.py
+++ baekjoon/8주차/16236/16236_jy.py
@@ -19,8 +19,6 @@ for i in range(N):
         if temp[j]:
             fish_nums += 1
             continue
-print(grids)
-print(fish_nums)
 
 # 근처 물고기들 중 가장 가까운 물고기 찾기
 def find_min_fish(cur_r, cur_c, shark_w):
@@ -29,8 +27,10 @@ def find_min_fish(cur_r, cur_c, shark_w):
     queue = deque()
     queue.append((cur_r, cur_c, 0)) # 현재 위치, 거리
     can_eat = []
+    min_ = sys.maxsize
     while queue:
         r, c, distance = queue.popleft()
+        if distance > min_: break
 
         for i in range(4):
             next_r, next_c = r+dir_[i][0], c+dir_[i][1]
@@ -38,11 +38,13 @@ def find_min_fish(cur_r, cur_c, shark_w):
                 if not visited[next_r][next_c] and grids[next_r][next_c] <= shark_w: # 아기 상어가 지나갈 수 있는 곳이면 
                     visited[next_r][next_c] = 1
                     queue.append((next_r, next_c, distance+1))
-                if 0 < grids[next_r][next_c] < shark_w and (next_r, next_c, distance+1) not in can_eat: # 아기 상어가 먹을 수 있는 물고기이면
+                if  0 < grids[next_r][next_c] < shark_w and min_ >= distance+1: # 아기 상어가 먹을 수 있는 물고기이면
+                    min_ = distance+1
                     can_eat.append((next_r, next_c, distance+1))
-    
-    can_eat = sorted(can_eat, key=lambda x: (x[2], x[0], x[1])) # -인 이유 -> 가장 위쪽 -> 가장 왼쪽
-    print("can_eat ; ",can_eat)
+                
+    print("can_eat 1 ; ",can_eat)
+    can_eat = sorted(set(can_eat), key=lambda x: (x[0], x[1])) # -인 이유 -> 가장 위쪽 -> 가장 왼쪽
+    print("can_eat 2 ; ",can_eat)
     if can_eat:
         return can_eat[0][0], can_eat[0][1], can_eat[0][2]
     else: return False
@@ -55,7 +57,7 @@ if __name__ == "__main__":
     while fish_nums:
         result = find_min_fish(shark_r, shark_c, shark_w)
         if result: # 먹을게 있다면
-            print("result : ", result)
+            #print("result : ", result)
             shark_r, shark_c= result[0], result[1] # 마지막 아기상어 위치 update
             total_dis += result[2] # 거리=> 이동 시간의미하므로 update
             fish_nums -= 1 # 물고기 한마리 냠 했으니 물고기 수 줄어듬
@@ -67,5 +69,5 @@ if __name__ == "__main__":
         else: # 먹을게 없으면 엄마상어한테 이르러감
             print(total_dis)
             break
-    if not fish_nums: print("ㅇㅇㅇ",total_dis)
+    if not fish_nums: print(total_dis)
 
