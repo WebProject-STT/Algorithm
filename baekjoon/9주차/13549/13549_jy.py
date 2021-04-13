86c9bd961c7be0ecefe14d2a6a4fea20c8980fdb
Juyoung4 <vallot7@naver.com>
2021-04-14 00:20:45 +0900

    [EDIT] 💌 baekjoon_13546.숨바꼭질3

--- baekjoon/9주차/13549/13549_jy.py
+++ baekjoon/9주차/13549/13549_jy.py
@@ -20,18 +20,18 @@ def BFS(N, K):
                 else:
                     visited[next_x] = visited[X] + 1
                     queue.append(next_x)
-        # if X*2 < 100001:
-        #     if not visited[X*2] and X != 0:
-        #         queue.appendleft(X*2) # X*2가 더 높은 순위를 가져야 빠르게 출력해서 나갈 수 있기 때뮨이다
+        
+        # # 수빈이가 이동할 수 있는 방법은 3가지
+        # if 0 <= X*2 < 100001 and not visited[X*2]:
+        #     if X != 0:
         #         visited[X*2] = visited[X]
-        # if X+1 < 100001:
-        #     if not visited[X+1]:
-        #         queue.append(X+1)
-        #         visited[X+1] = visited[X]+1
-        # if 0 <= X-1:
-        #     if not visited[X-1]:
-        #         queue.append(X-1)
+        #         queue.appendleft(X*2)
+        # if 0 <= X-1 < 100001 and not visited[X-1]:
         #         visited[X-1] = visited[X]+1
+        #         queue.append(X-1)
+        # if 0 <= X+1 < 100001 and not visited[X+1]:
+        #         visited[X+1] = visited[X]+1
+        #         queue.append(X+1)
                         
 N, K = map(int, sys.stdin.readline().split())
 visited = [0 for i in range(100001)]
