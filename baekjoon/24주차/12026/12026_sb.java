5f74d9b89e76092e31329cc58f2a2ae4b0417d78
Kim-SuBin <sb2504@naver.com>
2021-08-05 21:36:20 +0900

    [Solving] ... 푸는 중

---
+++ baekjoon/24주차/12026/12026_sb.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/** 백준 12026.BOJ 거리
+ * BOJ 순서로 블록 밟으면서 점프
+ * 입력 : 링크 집 위치 N, 1번부터 쓰여진 보도블록 글자. 1번은 B
+ * 출력 :스타트가 링크를 만나는데 필요한 에너지 최솟값 출력. 없을 경우 -1
+ * */
+class B12026 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+    }
+}
